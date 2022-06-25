1d0b38c268eaded035aaa97389d92a78d7c1ffe1
brandontaylor156 <brandon.david4@outlook.com>
2022-06-25 07:00:45 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function logOut(element) {
+    element.innerText = "Logout";
+}
+
+function removeButton(element) {
+    element.remove();
+}
+
+function ninjaLike() {
+    alert("Ninja was liked");
+}
